b848fc428eb68eeda8dca8cc3634782bd3b51698
Hugh Evans <hugh@artpop.com.au>
2008-07-14 21:48:34 +1000

    Adding skeleton rails app with the beginnings of a design

---
+++ app/controllers/application.rb
@@ -0,0 +1,19 @@
+# Filters added to this controller apply to all controllers in the application.
+# Likewise, all the methods added will be available for all controllers.
+
+class ApplicationController < ActionController::Base
+  helper :all # include all helpers, all the time
+
+  # See ActionController::RequestForgeryProtection for details
+  # Uncomment the :secret if you're not using the cookie session store
+  protect_from_forgery # :secret => '505bd66a8842a7f247596f00b5637871'
+  
+  # See ActionController::Base for details 
+  # Uncomment this to filter the contents of submitted sensitive data parameters
+  # from your application log (in this case, all fields with names like "password"). 
+  # filter_parameter_logging :password
+  
+  def testing
+  end
+  
+end
