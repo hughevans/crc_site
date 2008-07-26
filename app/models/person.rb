918a6d22b7902f0ed7253ad92faa40ba4e429e36
Michael MacDonald <michael@starclass.com.au>
2008-07-26 16:51:09 +1000

    added email validation

--- app/models/person.rb
+++ app/models/person.rb
@@ -3,5 +3,6 @@ class Person < ActiveRecord::Base
   validates_length_of     :email, :within  => 6..250
   validates_uniqueness_of :email, :case_sensitive => false,
                                   :message => 'is already registered'
+  validates_email_format_of :email
   
 end
