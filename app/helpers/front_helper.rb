8a2f461d9ae2b227e3a98272cc7b7d93aa34db6b
Tim Riley <tim@openmonkey.com>
2008-10-16 22:19:12 +1100

    move schlick's next meeting helper into the FrontHelper

--- app/helpers/front_helper.rb
+++ app/helpers/front_helper.rb
@@ -1,2 +1,12 @@
 module FrontHelper
+  # TODO: should be made customisable
+  MEETING_WDAY = 3        # Wednesday
+  
+  # returns the date for the next meeting
+  def next_meeting_date
+    last_day_of_month = Date.today.end_of_month
+    total = last_day_of_month.wday + MEETING_WDAY + 1
+    total -= 7 if total >= 7
+    return last_day_of_month - total.days
+  end  
 end
