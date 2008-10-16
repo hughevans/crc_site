0b993459da5e0b705d19c5a43590a015f800c693
Tim Riley <tim@openmonkey.com>
2008-10-16 22:08:59 +1100

    change the twitter feed to retrieve just the latest 10 items, and add some methods to show the username and full name separately for each tweet

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,11 +1,8 @@
-require 'open-uri'
-require 'ostruct'
-
 class Tweet < OpenStruct
   def self.all
     users         = Person.all(:select => 'twitter_user', :conditions => 'twitter_user IS NOT NULL')
     query_string  = users.inject('q=') { |str, person| str += "from%3A#{person.twitter_user}+OR+" }
-    feed          = Hpricot(open("http://search.twitter.com/search.atom?#{query_string.gsub(/\+OR\+$/, '')}"))
+    feed          = Hpricot(open("http://search.twitter.com/search.atom?#{query_string.gsub(/\+OR\+$/, '')}&rpp=10"))
     
     (feed/'entry').map { |e| Tweet.new(
       :from => (e/'name').inner_html,
@@ -13,5 +10,13 @@ class Tweet < OpenStruct
       :link => (e/'link').first['href'],
       :date => Time.parse((e/'published').inner_html)
     )}
+  end
+  
+  def username
+    from.split(' ').first.strip
+  end
+  
+  def full_name
+    from.split(' ')[1..-1].join(' ').gsub(/\(|\)/, '')
   end
 end
