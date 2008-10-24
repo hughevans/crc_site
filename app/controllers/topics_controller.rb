73b80e3578ae1f2a203dfec41478e1f3a96b49f0
Michael MacDonald <michaelm@amc.org.au>
2008-10-25 01:22:56 +1100

    added some ajax goodness to the meetings show page. its still ugly but its a start

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,35 +1,18 @@
 class TopicsController < ApplicationController
+  
   # GET /topics
-  # GET /topics.xml
   def index
     @topics = Topic.find(:all)
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.xml  { render :xml => @topics }
-    end
   end
 
   # GET /topics/1
-  # GET /topics/1.xml
   def show
     @topic = Topic.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.xml  { render :xml => @topic }
-    end
   end
 
   # GET /topics/new
-  # GET /topics/new.xml
   def new
     @topic = Topic.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.xml  { render :xml => @topic }
-    end
   end
 
   # GET /topics/1/edit
@@ -38,48 +21,35 @@ class TopicsController < ApplicationController
   end
 
   # POST /topics
-  # POST /topics.xml
   def create
     @topic = Topic.new(params[:topic])
-
     respond_to do |format|
       if @topic.save
         flash[:notice] = 'Topic was successfully created.'
-        format.html { redirect_to(@topic) }
-        format.xml  { render :xml => @topic, :status => :created, :location => @topic }
+        # redirect_to(@topic)
+        format.html { redirect_to meeting_path(@topic.meeting_id) }
+        format.js
       else
-        format.html { render :action => "new" }
-        format.xml  { render :xml => @topic.errors, :status => :unprocessable_entity }
+        render :action => "new"
       end
     end
   end
 
   # PUT /topics/1
-  # PUT /topics/1.xml
   def update
     @topic = Topic.find(params[:id])
-
-    respond_to do |format|
-      if @topic.update_attributes(params[:topic])
-        flash[:notice] = 'Topic was successfully updated.'
-        format.html { redirect_to(@topic) }
-        format.xml  { head :ok }
-      else
-        format.html { render :action => "edit" }
-        format.xml  { render :xml => @topic.errors, :status => :unprocessable_entity }
-      end
+    if @topic.update_attributes(params[:topic])
+      flash[:notice] = 'Topic was successfully updated.'
+      redirect_to(@topic)
+    else
+      render :action => "edit"
     end
   end
 
   # DELETE /topics/1
-  # DELETE /topics/1.xml
   def destroy
     @topic = Topic.find(params[:id])
     @topic.destroy
-
-    respond_to do |format|
-      format.html { redirect_to(topics_url) }
-      format.xml  { head :ok }
-    end
+    redirect_to(topics_url)
   end
 end
