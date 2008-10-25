df6c13e0467972927abf7219b16399ab7ffba02e
Michael MacDonald <michaelm@amc.org.au>
2008-10-26 01:02:15 +1100

    added two extra date formats, moved ajax toggle to head, added in toggling of link label text, fixed display of topics without a link

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,3 +1,6 @@
 class Topic < ActiveRecord::Base
   belongs_to :meeting
+  
+  validates_presence_of :title
+  validates_presence_of :description
 end
---
+++ config/initializers/date_formats.rb
@@ -0,0 +1,4 @@
+ActiveSupport::CoreExtensions::Time::Conversions::DATE_FORMATS.merge!(
+  :day_date => "%A, %e %B %Y",
+  :am_pm  => "%I:%M%p"
+)
