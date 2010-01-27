f63953ea6d0f65f35530b38cd0036cca10d25b98
Hugh Evans <hugh@artpop.com.au>
2010-01-27 20:07:59 +1100

    Rails 2.3.5, gem bundler and Gravtastic hack

---
+++ Gemfile
@@ -0,0 +1,9 @@
+bundle_path 'vendor/bundler_gems'
+
+clear_sources
+source 'http://gemcutter.org'
+
+gem 'bj'
+gem 'json'
+gem 'gravtastic'
+gem 'hpricot'
--- app/models/person.rb
+++ app/models/person.rb
@@ -1,7 +1,8 @@
 class Person < ActiveRecord::Base
+  include Gravtastic
 
-  has_gravatar
-  
+  is_gravtastic!
+
   validates_length_of     :email, :within  => 6..250
   validates_uniqueness_of :email, :case_sensitive => false,
                                   :message => 'is already registered'
